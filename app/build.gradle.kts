cc6f9b6c346ca9174023d69753bba6fe5b58914e
Daniel Olovianniy <daniel.olovianniy@gmail.com>
2026-06-02 02:22:40 +0300

    feat: configure detekt properly

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.detekt)
 }
 
 android {
@@ -37,6 +38,10 @@ android {
     buildFeatures {
         compose = true
     }
+}
+
+detekt {
+    config.setFrom(file("${rootDir}/config/detekt/detekt.yml"))
 }
 
 dependencies {
