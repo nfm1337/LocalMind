aa6f91673fb4cdd0984752ef7235e36940f52894
Daniel Olovianniy <daniel.olovianniy@gmail.com>
2026-06-24 13:30:38 +0300

    ci: align commit message validation

--- scripts/validate-commit-msg.sh
+++ scripts/validate-commit-msg.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 commit_msg=$(cat "$1")
-pattern='^(feat|fix|refactor|chore|docs|test|style|ci)(\(.+\))?: .{1,72}$'
+pattern='^(feat|fix|docs|style|refactor|perf|test|chore|ci)(\(.+\))?: .{1,72}$'
 
 if ! echo "$commit_msg" | grep -qE "$pattern"; then
   echo "Commit message must follow Conventional Commits:"
